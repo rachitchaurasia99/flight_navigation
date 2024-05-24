70392d1129e89e9b63ad334803d7ce3ce31f7758
Rachit Chaurasia <rachit.chaurasia@snackmagic.com>
2024-05-25 03:49:14 +0530

    shortest path algo

--- db/migrations/create_cities_table.js
+++
@@ -1,8 +0,0 @@
-const createCitiesTable = `
-    CREATE TABLE IF NOT EXISTS cities (
-        id INT PRIMARY KEY,
-        name VARCHAR(255) NOT NULL
-    );
-`;
-
-export default createFlightsTable;
--- db/migrations/create_flights_table.js
+++
@@ -1,8 +0,0 @@
-const createFlightsTable = `
-    CREATE TABLE IF NOT EXISTS flights (
-        id INT PRIMARY KEY,
-        number VARCHAR(255) NOT NULL
-    );
-`;
-
-export default createFlightsTable;
